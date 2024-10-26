db526ff407480e859d48605244696636fe9d7554
jan <jas.gol@poczta.onet.pl>
2024-10-26 21:14:45 +0200

    build Gameboard functions

--- script.js
+++ script.js
@@ -4,15 +4,59 @@ function Gameboard(){
     [null, null, null],
     [null, null, null]];
 
-    function checkIfGbFull(){}
+    function checkIfGbFull(){
+        for(row of gameboard)
+            for(e of row)
+                if(e === null)  return false;
+        return true;
+    }
     
-    function checkRow(){}
+    function checkRow(rowNumber, symbol){
+        for(let i=0; i<3; i++){
+            if(gameboard[rowNumber][i] !== symbol)  return false;
+        }
+        return true;
+    }
 
-    function checkCollumn(){}
+    function checkCollumn(columnNumber, symbol){
+        for(let i=0; i<3; i++){
+            if(gameboard[i][columnNumber] !== symbol)   return false;
+        }
+        return true;
+    }
 
-    function checkTies(){}
+    function checkTies(symbol){
+        let firstTie, secondTie = true;
 
-    return {};
+        for(let i=0; i<3; i++){
+            if(gameboard[i][i] !== symbol){
+                firstTie=false;
+                break;
+            }
+        }
+
+        for(let i=0; i<3; i++){
+            if(gameboard[2-i][i] !== symbol){
+                secondTie=false;
+                break;
+            }
+        }
+
+        return firstTie || secondTie;
+    }
+
+    function printGameboard(){
+        let message = "";
+        for(row of gameboard){
+            for(e of row){
+                message += e+" ";
+            }
+            message += "\n";
+        }
+        console.log(message);
+    }
+
+    return {checkIfGbFull, checkRow, checkCollumn, checkTies, printGameboard};
 }
 
 function Player(){
