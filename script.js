ee018099e589c51bf2a0e31a15c8c625692237af
jan <jas.gol@poczta.onet.pl>
2024-10-27 00:47:51 +0200

    write GameController functions i guess

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function Gameboard(){
-    gameboard = 
+    let gameboard = 
     [[null, null, null],
     [null, null, null],
     [null, null, null]];
+
+    function mark(symbol, row, column){
+        if(row>=0 && row<=2 && column>=0 && column<=2 && gameboard[row][column] === null){
+            gameboard[row][column]=symbol;
+            return "added mark";
+        }
+        else    
+            return "error";
+    }
 
     function checkIfGbFull(){
         for(row of gameboard)
@@ -18,7 +27,7 @@ function Gameboard(){
         return true;
     }
 
-    function checkCollumn(columnNumber, symbol){
+    function checkColumn(columnNumber, symbol){
         for(let i=0; i<3; i++){
             if(gameboard[i][columnNumber] !== symbol)   return false;
         }
@@ -26,7 +35,8 @@ function Gameboard(){
     }
 
     function checkTies(symbol){
-        let firstTie, secondTie = true;
+        let firstTie = true;
+        let secondTie = true;
 
         for(let i=0; i<3; i++){
             if(gameboard[i][i] !== symbol){
@@ -56,7 +66,7 @@ function Gameboard(){
         console.log(message);
     }
 
-    return {checkIfGbFull, checkRow, checkCollumn, checkTies, printGameboard};
+    return {checkIfGbFull, checkRow, checkColumn, checkTies, printGameboard, mark};
 }
 
 function Player(){
@@ -66,15 +76,69 @@ function Player(){
     return {name, symbol};
 }
 
-function gameController(){
-    let player1, player2;
-    let activePlayer;
+function GameController(){
+    const gameboard = Gameboard();
 
-    function getUserMove(){}
+    let player1 = Player();
+    player1.name = 'firstPlayer';
+    player1.symbol = 'X';
 
-    function checkForGameResult(){}
+    let player2 = Player();
+    player2.name = 'secondPlayer';
+    player2.symbol = 'O';
 
-    function announceGameResult(){}
+    let lastMove = {row: -1, column: -1};
+    let isGameUp = true;
+
+    let activePlayer = player1;
+
+    function getAndMarkSymbol(){
+        let message;
+        do{
+            alert(activePlayer.name+" mark "+activePlayer.symbol+" on gameboard");
+            lastMove.column = Number(prompt("x: "));
+            lastMove.row = Number(prompt("y: "));
+            message =  gameboard.mark(activePlayer.symbol, lastMove.row, lastMove.column);
+            gameboard.printGameboard();
+        }
+        while(message === "error");
+    }
+
+    function checkForGameResult(){
+        console.log(lastMove);
+        if(
+            gameboard.checkColumn(lastMove.column,activePlayer.symbol) ||
+            gameboard.checkRow(lastMove.row, activePlayer.symbol) ||
+            gameboard.checkTies(activePlayer.symbol)
+        )
+            return activePlayer.name;
+        else if(gameboard.checkIfGbFull())
+            return "draw";
+        else 
+            return false;
+    }
+
+    function announceGameResult(result){
+        if(result === "draw")   console.log(draw);
+        else    console.log('Congratulations: '+activePlayer.name+' you win!');
+    }
+
+    function changeActivePlayer(){
+        activePlayer = activePlayer === player1 ? player2 : player1;
+    }
+
+    let gameResult;
+    while(isGameUp){
+        getAndMarkSymbol();
+        result = checkForGameResult();
+        if(result){
+            announceGameResult(gameResult);
+            break;
+        }
+        changeActivePlayer();
+    }
+
+    return {getAndMarkSymbol}
 }
 
 function uiController(){}
