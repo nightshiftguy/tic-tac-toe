a6ea7fd09216a00b78a01e1f32277c37d7b6d829
jan <jas.gol@poczta.onet.pl>
2024-10-26 20:43:51 +0200

    create inital js structure

---
+++ script.js
@@ -0,0 +1,36 @@
+function Gameboard(){
+    gameboard = 
+    [[null, null, null],
+    [null, null, null],
+    [null, null, null]];
+
+    function checkIfGbFull(){}
+    
+    function checkRow(){}
+
+    function checkCollumn(){}
+
+    function checkTies(){}
+
+    return {};
+}
+
+function Player(){
+    let name = "";
+    let symbol = "";
+
+    return {name, symbol};
+}
+
+function gameController(){
+    let player1, player2;
+    let activePlayer;
+
+    function getUserMove(){}
+
+    function checkForGameResult(){}
+
+    function announceGameResult(){}
+}
+
+function uiController(){}
